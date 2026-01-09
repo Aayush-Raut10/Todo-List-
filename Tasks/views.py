26ed335f0428471fa3f0aba3c7312de1d06ee8a5
Aayush-Raut10 <aayushraut209@gmail.com>
2026-01-09 17:46:39 +0545

    implementing read and create task

--- Tasks/views.py
+++ Tasks/views.py
@@ -1,18 +1,26 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from Tasks.models import Tasks
 
 # Create your views here.
 def index(request):
     
     if request.method == "POST":
-        task = request.POST.get("task")
+        task = request.POST.get("title")
 
         print(task)
 
-        
-       
+        newTask = Tasks(title=task)
+        newTask.save()
 
+        
         return redirect('index')   #  IMPORTANT
 
-  
-    return render(request, "Tasks/index.html")
+    else:
+
+        tasks = Tasks.objects.all()
+
+        context = {
+            "tasks":tasks,
+        }
+    return render(request, "Tasks/index.html", context=context)
