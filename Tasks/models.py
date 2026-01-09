a30a119846b1df5e05a4e63e45d9345fa52842dd
Aayush-Raut10 <aayushraut209@gmail.com>
2026-01-09 17:34:18 +0545

    Create Tasks DB

--- Tasks/models.py
+++ Tasks/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Tasks(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- Tasks/views.py
+++ Tasks/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
+    
+    if request.method == "POST":
+        task = request.POST.get("task")
+
+        print(task)
+
+        
+       
+
+        return redirect('index')   #  IMPORTANT
+
+  
     return render(request, "Tasks/index.html")
